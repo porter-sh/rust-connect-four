bd63a8e8ea6ec60a08554786b519aa64a21d686c
Alex Broihier <alexanderbroihier@gmail.com>
2022-11-22 17:11:20 -0600

    Added functionality to ConnectionProtocol to aid with networking

--- constants/src/lib.rs
+++ constants/src/lib.rs
@@ -16,6 +16,9 @@ impl ConnectionProtocol {
 
     pub const IS_PLAYER_1: u8 = 254;
     pub const IS_PLAYER_2: u8 = 253;
+    pub const IS_SPECTATOR: u8 = 252;
+
+    pub const SPECIAL_MESSAGE: u64 = 1 << (2 * BOARD_HEIGHT + 1);
 
     pub const COL_0: u8 = 0;
     pub const COL_1: u8 = 1;
@@ -24,4 +27,22 @@ impl ConnectionProtocol {
     pub const COL_4: u8 = 4;
     pub const COL_5: u8 = 5;
     pub const COL_6: u8 = 6;
+
+    pub fn assemble_message(bytes: Vec<u8>) -> Result<u64, ()> {
+        if bytes.len() != 7 { return Err(()); }
+        let mut msg = 0;
+        for i in 0..7 {
+            msg |= (bytes[i] as u64) << (i as u8 * (BOARD_HEIGHT + 1));
+        }
+        Ok(msg)
+    }
+
+    pub fn disassemble_message(msg: u64) -> Vec<u8> {
+        const MAX_U8: u64 = 255;
+        let mut bytes = Vec::with_capacity(7);
+        for i in 0..7 {
+            bytes.push(((msg >> (i * 8)) & MAX_U8) as u8);
+        }
+        bytes
+    }
 }
